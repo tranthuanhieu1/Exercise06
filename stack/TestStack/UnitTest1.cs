9e723142116a48a965891f6c9ead99f04d239229
phamvietdung254 <vietdungpham25499@gmail.com>
2019-10-24 15:28:15 +0700

    Stack

---
+++ stack/TestStack/UnitTest1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using stack;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class StackTest
+    {
+        [TestMethod]
+        public void CreatedMyStack()
+        {
+            Stack s1 = new Stack(10);
+            s1.Put(1);
+            s1.Put(2);
+            s1.Put(3);
+            s1.Put(4);
+            Assert.AreEqual(4, s1.Count());
+        }
+        [TestMethod]
+        public void checkPop()
+        {
+            Stack s2 = new Stack(10);
+            s2.Put(1);
+            s2.Put(2);
+            s2.Put(3);
+            s2.Put(4);
+            Assert.AreEqual(4, s2.Pop());
+        }
+        [TestMethod]
+        public void checkPeek()
+        {
+            Stack s3 = new Stack(10);
+            s3.Put(1);
+            s3.Put(2);
+            s3.Put(3);
+            s3.Put(4);
+            Assert.AreEqual(4, s3.Peek());
+        }
+    }
+}
---
+++ stack/stack/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stack
+{
+    public class Stack
+    {
+        private int [] Buffer;
+        private int Top;
+         public Stack(string value)
+         {
+             Buffer = new int[NotFiniteNumberException;
+             Top = -1;
+         }
+        public void Put(int item)
+        {
+            Buffer[Top+1]= item;
+            Top++;
+        }
+         public int Pop()
+         {
+             string number = Buffer[Top];
+             Top--;
+             return Buffer[Top];
+         }
+        public int Peek()
+        {
+            return Buffer[Top];
+        }
+        public int Count()
+        {
+           return Top;
+        }
+      
+    }
+}
